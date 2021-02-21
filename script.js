a1a997894d90c9097fc425100f946f000ad1dcc3
Corrina Lichthardt <corrina.lichthardt@mediawave.de>
2021-02-21 20:02:56 +0100

    coordinate change

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const loadPlaces = function(coords) {
     {
       name: "Hello there",
       location: {
-        lat: coords.latitude + 0.00001,
-        lng: coords.longitude + 0.00001
+        lat: coords.latitude + 0.00002,
+        lng: coords.longitude + 0.00002
       }
       // name: "Marienplatz",
       // location: {
@@ -45,7 +45,7 @@ window.onload = () => {
             "https://mwclichthardt.github.io/location-based-app/assets/cat.png"
           );
           // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-          icon.setAttribute("scale", "10, 10");
+          icon.setAttribute("scale", "8, 8");
 
           icon.addEventListener("loaded", () =>
             window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"))
